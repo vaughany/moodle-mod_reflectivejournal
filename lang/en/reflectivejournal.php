83153049c25c8bde3b05785b21b9d756acfde322
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2012-03-16 14:37:02 +0000

    Modified some of the language strings.

--- lang/en/reflectivejournal.php
+++ lang/en/reflectivejournal.php
@@ -23,45 +23,45 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['accessdenied']     = 'Access denied';
-$string['alwaysopen']       = 'Always open';
-$string['blankentry']       = 'Blank entry';
-$string['daysavailable']    = 'Days available';
-$string['editingended']     = 'Editing period has ended';
-$string['editingends']      = 'Editing period ends';
-$string['entries']          = 'Entries';
-$string['feedbackupdated']  = 'Feedback updated for {$a} entries';
-$string['gradeingradebook'] = 'Current grade in gradebook';
-$string['journal:addentries']       = 'Add reflective journal entries';
-$string['journal:manageentries']    = 'Manage reflective journal entries';
-$string['journalmail']      = '{$a->teacher} has posted some feedback on your
+$string['accessdenied']                 = 'Access denied';
+$string['alwaysopen']                   = 'Always open';
+$string['blankentry']                   = 'Blank entry';
+$string['daysavailable']                = 'Days available';
+$string['editingended']                 = 'Editing period has ended';
+$string['editingends']                  = 'Editing period ends';
+$string['entries']                      = 'Entries';
+$string['feedbackupdated']              = 'Feedback updated for {$a} entries';
+$string['gradeingradebook']             = 'Current grade in gradebook';
+$string['reflectivejournal:addentries']       = 'Add reflective journal entries';
+$string['reflectivejournal:manageentries']    = 'Manage reflective journal entries';
+$string['reflectivejournalmail']        = '{$a->teacher} has posted some feedback on your
 reflective journal entry for \'{$a->journal}\'
 
 You can see it appended to your journal entry:
 
     {$a->url}';
-$string['journalmailhtml']  = '{$a->teacher} has posted some feedback on your
+$string['reflectivejournalmailhtml']    = '{$a->teacher} has posted some feedback on your
 reflective journal entry for \'<i>{$a->journal}</i>\'<br /><br />
 You can see it appended to your <a href=\"{$a->url}\">journal entry</a>.';
-$string['journalname']      = 'Reflective Journal name';
-$string['journalquestion']  = 'Reflective Journal question';
-$string['mailsubject']      = 'Reflective Journal feedback';
-$string['modulename']       = 'Reflective Journal';
-$string['modulenameplural'] = 'Reflective Journals';
-$string['newjournalentries']        = 'New journal entries';
-$string['noentriesmanagers']        = 'There are no teachers';
-$string['noentry']          = 'No entry';
-$string['noratinggiven']    = 'No rating given';
-$string['notopenuntil']     = 'This journal won\'t be open until';
-$string['notstarted']       = 'You have not started this journal yet';
-$string['overallrating']    = 'Overall rating';
-$string['pluginadministration']     = 'Journal module administration';
-$string['pluginname']       = 'Reflective Journal';
-$string['rate']             = 'Rate';
-$string['removeentries']    = 'Remove all entries';
-$string['saveallfeedback']  = 'Save all my feedback';
-$string['showrecentactivity']       = 'Show recent activity';
-$string['showoverview']     = 'Show reflective journals overview on my Moodle';
-$string['startoredit']      = 'Start or edit my reflective journal entry';
-$string['viewallentries']   = 'View {$a} reflective journal entries';
-$string['viewentries']      = 'View entries';
+$string['reflectivejournalname']        = 'Reflective Journal name';
+$string['reflectivejournalquestion']    = 'Reflective Journal question';
+$string['mailsubject']                  = 'Reflective Journal feedback';
+$string['modulename']                   = 'Reflective Journal';
+$string['modulenameplural']             = 'Reflective Journals';
+$string['newreflectivejournalentries']        = 'New journal entries';
+$string['noentriesmanagers']            = 'There are no teachers';
+$string['noentry']                      = 'No entry';
+$string['noratinggiven']                = 'No rating given';
+$string['notopenuntil']                 = 'This journal won\'t be open until';
+$string['notstarted']                   = 'You have not started this journal yet';
+$string['overallrating']                = 'Overall rating';
+$string['pluginadministration']         = 'Journal module administration';
+$string['pluginname']                   = 'Reflective Journal';
+$string['rate']                         = 'Rate';
+$string['removeentries']                = 'Remove all entries';
+$string['saveallfeedback']              = 'Save all my feedback';
+$string['showrecentactivity']           = 'Show recent activity';
+$string['showoverview']                 = 'Show reflective journals overview on my Moodle';
+$string['startoredit']                  = 'Start or edit my reflective journal entry';
+$string['viewallentries']               = 'View {$a} reflective journal entries';
+$string['viewentries']                  = 'View entries';
